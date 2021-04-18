9faf15f50c007718cb2156d5465dce532e9a8e25
ramakrishna0505 <ramakrishna1870@gmail.com>
2021-04-18 05:51:54 +0530

    movie search implementation

---
+++ index.js
@@ -0,0 +1,75 @@
+const inputSearch = document.querySelector('#input-search');
+const movieContainer = document.querySelector('.movie-container'); 
+let userSearched = !!sessionStorage.getItem('userSearched');
+inputSearch.value = sessionStorage.getItem('searchText');
+function debounce(callback, delay) {
+    let timerId;
+    return (...args) => {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            callback(...args);
+        }, delay);
+    }
+}
+
+inputSearch.addEventListener('keyup', debounce(getMovies, 500));
+inputSearch.addEventListener('search', ()=>{
+    movieContainer.innerHTML = '';
+    sessionStorage.setItem('searchText', '');
+});
+
+function getMovies(e) {
+    let value;
+    if(typeof e === 'string'){
+        value = e;
+    } else {
+        value = e.target.value;
+    }
+    sessionStorage.setItem('searchText', value);
+    sessionStorage.setItem('userSearched', true);
+    if(value.length > 2) {
+        const url = `http://www.omdbapi.com/?s=${value}&apikey=a5a3bcde`;
+        fetch(url)
+        .then(response => response.json())
+        .then(moviesObj => {
+            const {Search, Response, Error} = moviesObj;
+            if(Response === 'True'){
+                showMovies(Search);
+            } else {
+                movieContainer.innerHTML = Error;
+            }
+        })
+    } else {
+        movieContainer.innerHTML = '';
+    }
+}
+
+
+const showMovies = (movies) => {
+    movieContainer.innerHTML = '';
+    movies.forEach((movie) => {
+        movieContainer.appendChild(movieCard(movie));
+    })
+}
+
+
+const movieCard = (movie) => {
+    const { Title, Year, Poster, imdbID} = movie;
+    const movieDiv = document.createElement('div');
+    const moviePoster = document.createElement('img');
+    movieDiv.classList.add('movie-card');
+    movieDiv.addEventListener('click', function (){
+        location.href = `movie.html?id=${imdbID}`;
+    })
+    if(Poster !== 'N/A') moviePoster.setAttribute('src', Poster);
+    else moviePoster.setAttribute('alt', 'No image found');
+    const movieTitle = document.createElement('div');
+    movieTitle.textContent = `${Title} - ${Year}`;
+    movieDiv.appendChild(moviePoster);
+    movieDiv.appendChild(movieTitle);
+    return movieDiv;
+}
+
+const searchedText = sessionStorage.getItem('searchText');
+if(userSearched && searchedText) getMovies(searchedText);
+
---
+++ movie.js
@@ -0,0 +1,54 @@
+const imdbId = location.search.split('=')[1];
+
+
+const getMovieDetails = (id) => {
+    const url = `http://www.omdbapi.com/?i=${id}&apikey=a5a3bcde`;
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => showMovieDetails(data));
+}
+
+getMovieDetails(imdbId);
+
+const showMovieDetails = (movieObj) => {
+    const {Poster, Actors, Awards, BoxOffice, Country, Director, Title,
+         Genre, Language, Plot, Rated, Runtime, Year, imdbRating, imdbVotes } = movieObj;
+    const posterParent = document.querySelector('#poster');
+    const contentParent = document.querySelector('#content');
+    const moviePoster = document.createElement('img');
+    if(Poster !== 'N/A') moviePoster.setAttribute('src', Poster);
+    else moviePoster.setAttribute('alt', 'No image found');
+    posterParent.appendChild(moviePoster);
+    contentParent.appendChild(getDiv('Title', Title));
+    contentParent.appendChild(getDiv('Actors', Actors));
+    contentParent.appendChild(getDiv('Awards', Awards));
+    contentParent.appendChild(getDiv('Box Office', BoxOffice));
+    contentParent.appendChild(getDiv('Country', Country));
+    contentParent.appendChild(getDiv('Director(s)', Director));
+    contentParent.appendChild(getDiv('Genre', Genre));
+    contentParent.appendChild(getDiv('Language', Language));
+    contentParent.appendChild(getDiv('Rated', Rated));
+    contentParent.appendChild(getDiv('Runtime', Runtime));
+    contentParent.appendChild(getDiv('Year', Year));
+    contentParent.appendChild(getDiv('Plot', Plot));
+    contentParent.appendChild(getDiv('IMDb Rating', imdbRating));
+    contentParent.appendChild(getDiv('IMDb Votes', imdbVotes));
+}
+
+const getDiv = (label, value) => {
+  const div = document.createElement('div');
+  div.classList.add('flex');
+  div.style.marginBottom = '4px';
+  const title = document.createElement('strong');
+  title.innerText = label;
+  title.style.width = '120px';
+  title.style.color = '#eaa917';
+  const valueDiv = document.createElement('div');
+  valueDiv.setAttribute('id', 'valueDiv');
+  valueDiv.style.marginLeft = '4px';
+  valueDiv.innerText = value || 'N/A';
+  valueDiv.style.maxWidth = '680px';
+  div.appendChild(title);
+  div.appendChild(valueDiv)
+  return div;
+}
